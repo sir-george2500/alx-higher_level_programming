245af77f74bc40cb53bd22b91e9f7082d1a5259b
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-10-24 01:18:47 +0000

    solve task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
-    """
-    Safely performs division of two numbers.
-
-    Args:
-        a (int, float): The dividend.
-        b (int, float): The divisor.
-
-    Returns:
-        float or None: The result of the division, or None in case of division by zero or floating point error.
-    """
     try:
         div = a / b
     except (ZeroDivisionError, FloatingPointError):
@@ -18,4 +7,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(div))
     return div
-
