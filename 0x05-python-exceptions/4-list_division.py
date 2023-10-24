27fc580d9f5a968385f240054b9d09286f6a0fe3
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-10-24 01:17:02 +0000

    solve task 4 again

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
-    """
-    Divides corresponding elements of two lists.
-
-    Args:
-        my_list_1 (list): The first list of numbers.
-        my_list_2 (list): The second list of numbers.
-        list_length (int): The number of elements to consider.
-
-    Returns:
-        list: A list containing the result of the division of corresponding elements.
-    """
     div = []
     temp_result = 0
     for i in range(0, list_length):
@@ -30,4 +18,3 @@ def list_division(my_list_1, my_list_2, list_length):
             pass
         div.append(temp_result)
     return div
-
