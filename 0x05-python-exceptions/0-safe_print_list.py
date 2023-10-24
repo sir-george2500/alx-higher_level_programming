b0853b0af84180b57ec93c9148b7309a84ad374c
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-10-24 01:28:04 +0000

    solve task 0 again

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """
-    Safely prints the first 'x' elements of 'my_list'.
-
-    Args:
-        my_list (list, optional): The list to be printed. Defaults to an empty list.
-        x (int, optional): The number of elements to print. Defaults to 0.
-
-    Returns:
-        int: The total number of elements printed.
-    """
     total = 0
     for i in range(x):
         try:
@@ -20,4 +10,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return total
-
