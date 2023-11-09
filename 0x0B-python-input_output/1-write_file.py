203ec212c5884fc7c23dc7a080317ac8321303c6
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-11-09 01:35:25 +0000

    solve tasks :rocket

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,7 +9,7 @@ def write_file(filename="", text=""):
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
     Returns:
-        The number of Characters Written
+        The number of characters written.
     """
-    with open(filename="", "w", encoding="UTF8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
